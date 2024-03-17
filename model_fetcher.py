40327f80db4a1708566438b513b3cd70488abe5f
PSRahul <psrahul.ml@gmail.com>
2024-03-17 11:10:39 +0100

    Sentence Classification

--- model_fetcher.py
+++ model_fetcher.py
@@ -3,8 +3,12 @@ from transformers import pipeline
 
 
 def main():
+
+    # Get an Text2Text Generator from Hugging Face Model
     model_generator = pipeline("text2text-generation", model="t5-base")
-    model_generator("question: How do I push models back to CPU in PyTorch?")
+
+    # Let's Try Sentence Classification
+    print(model_generator("sst2 sentence: Dune 2 is extraordinary!", max_length=20))
 
 
 if __name__ == "__main__":
